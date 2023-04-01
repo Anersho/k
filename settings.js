1724b75198452624f54675a88615f350be4eda49
Anersho <108976752+Anersho@users.noreply.github.com>
2023-04-01 17:47:46 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -54,23 +54,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.vcardowner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.vcardowner = ['918714093544'] //ur owner number
+global.ownername = "Anersh" //ur owner name
+global.ytname = "yt RC WIZARD" //ur yt chanel name
+global.socialm = "INSTA _.anersh._" //ur github or insta name
+global.location = "India,kerala" //ur location
 
 //bot bomdy 
-global.owner = ['916909137213']
-global.ownertag = '916909137213' //ur tag number
-global.botname = 'Cheems Bot MD V4' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
-global.reactmoji = "😘" //ur menu react emoji
-global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Cheems Bot Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
-global.author = "🦄Dream Guy Xeon" //ur sticker watermark author
+global.owner = ['918714093544']
+global.ownertag = '918714093544' //ur tag number
+global.botname = 'Bot RC WIZARD' //ur bot name
+global.linkz = "https://instagram.com/_.anersh._?igshid=ZDdkNTZiNTM=" //your theme url which will be displayed on whatsapp
+global.websitex = "https://instagram.com/_.anersh._?igshid=ZDdkNTZiNTM=" //ur website to be displayed
+global.botscript = 'https://instagram.com/_.anersh._?igshid=ZDdkNTZiNTM=' //script link
+global.reactmoji = "😧" //ur menu react emoji
+global.themeemoji = "😩" //ur theme emoji
+global.packname = "RC WIZARD stickers\n\n\n\n\n\n\n\n\n\n\n😂 " //ur sticker watermark packname
+global.author = "RC WIZARD" //ur sticker watermark author
 
 //Bot theme media
 global.thum = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['916909137213'] //ur premium numbers
+global.premium = ['918714093544'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
